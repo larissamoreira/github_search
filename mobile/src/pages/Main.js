ad7a55ef4f8563aa93dc73d7e8ae1f4bbe1b5a6d
Larissa Gusmao <larissamgusmao@gmail.com>
2020-01-30 20:08:17 -0500

    Adds react native

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+function Main({ navigation }) {
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if (granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04
+                })
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if (!currentRegion) {
+        return null;
+    }
+
+    return <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={currentRegion}>
+            <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/26466167?s=460&v=4' }} />
+
+            <Callout onPress={() => {
+                navigation.navigate('Profile', { github_username: 'larissamoreira' })
+            }}>
+                <View style={styles.callout}>
+                    <Text style={styles.devName}>Larissa Moreira</Text>
+                    <Text style={styles.devBio}>Web Developer Student</Text>
+                    <Text style={styles.devTechs}>React.JS, React Native, Node.js</Text>
+                </View>
+            </Callout>
+        </Marker>
+    </MapView>
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+    callout: {
+        width: 260,
+    },
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+    devTechs: {
+        marginTop: 5
+    }
+
+})
+
+export default Main;
